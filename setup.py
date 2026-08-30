071c11239cbead0064d9865c1d5fa03124b60264
Siddhant Roy <siddhantroy2006@gmail.com>
2026-08-30 16:11:33 +0530

    Add setup.py and update requirements.txt for package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+
+
+setup(
+    name="realtime-source-code-analyzer",
+    version="0.1.0",
+    author="Siddhant",
+    author_email="siddhantroy2006@gmail.com",
+    packages= find_packages(),
+    install_requires=[] 
+)
